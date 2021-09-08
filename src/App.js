92b584eb8d35230a134d0442e6da7dc5cda74936
Candinho Luiz Dalla Brida Junior <candinholuiz@gmail.com>
2021-09-08 01:09:39 -0300

    issue #3 - add material-ui redux e react-router libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 
-function App() {
+import Routes from "./routes";
+import AppTheme from "./theme/appTheme";
+import Header from "./components/Header";
+
+const applyTheme = createTheme(AppTheme);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={applyTheme}>
+        <Header />
+        <Routes />
+      </ThemeProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
